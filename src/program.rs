ff6f3173f53b92a8f13d9b086bbbda8ad0d3f7ed
nukeop <nukeop@gumblert.tech>
2018-11-12 02:18:52 +0100

    Write magic number and version to binary

--- src/program.rs
+++ src/program.rs
@@ -28,6 +28,18 @@ impl ZeusHeader {
             zero: [0; 25]
         }
     }
+
+    pub fn as_slice(&mut self) -> [u8; 32] {
+        let mut slice = [0; 32];
+        for i in 0..self.magic.len() {
+            slice[i] = self.magic[i];
+        }
+        slice[self.magic.len() + 1] = self.version.major;
+        slice[self.magic.len() + 2] = self.version.minor;
+        slice[self.magic.len() + 3] = self.version.patch;
+        
+        slice
+    }
 }
 
 pub struct Program {
@@ -45,6 +57,7 @@ impl Program {
 
     pub fn to_file(&mut self, filename: String) -> io::Result<()> {
         let mut file = File::create(filename)?;
+        file.write_all(&self.header.as_slice())?;
         file.write_all(&self.bytes)?;
         Ok(())
     }
