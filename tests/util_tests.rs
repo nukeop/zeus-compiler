c10b5018dcff949012c6d2f5df41cb77869830e5
nukeop <nukeop@gumblert.tech>
2018-11-23 10:55:05 +0100

    Update tests

--- tests/util_tests.rs
+++ tests/util_tests.rs
@@ -7,21 +7,21 @@ mod util_tests {
     #[test]
     fn unpack_u16() {
         let vector = (0xFFAA).as_u8_vec();
-        assert_eq!(vector[0], 0xFF);
-        assert_eq!(vector[1], 0xAA);
+        assert_eq!(vector[0], 0xAA);
+        assert_eq!(vector[1], 0xFF);
     }
 
     #[test]
     fn unpack_u16_low_zero() {
         let vector = (0x9900).as_u8_vec();
-        assert_eq!(vector[0], 0x99);
-        assert_eq!(vector[1], 0);
+        assert_eq!(vector[0], 0);
+        assert_eq!(vector[1], 0x99);
     }
 
     #[test]
     fn unpack_u16_hi_zero() {
         let vector = (0x0064).as_u8_vec();
-        assert_eq!(vector[0], 0);
-        assert_eq!(vector[1], 0x64);
+        assert_eq!(vector[0], 0x64);
+        assert_eq!(vector[1], 0);
     }
 }
