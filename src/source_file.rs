eaf78fb8b7e87302dde72e99eb90217ceabee474
nukeop <nukeop@gumblert.tech>
2018-12-03 02:09:08 +0100

    Implement compilation rules for all instructions

--- src/source_file.rs
+++ src/source_file.rs
@@ -29,7 +29,6 @@ impl Line {
 
     pub fn compile_n_addr(&mut self, addr_num: usize, bytes: &mut Vec<u8>) {
         bytes.push(self.op.opcode);
-        let addresses: Vec<u16> = vec!();
 
         for n in 0..addr_num {
             let addr = u16::from_str_radix(&self.args[n], 16).unwrap().as_u8_vec();
@@ -37,37 +36,7 @@ impl Line {
         }
     }
 
-    pub fn to_compiled(&mut self) -> Result<Vec<u8>, String> {
-        let mut result = vec!();
-        let name: &str = &self.op.name.to_owned();
-        
-        match name {
-            "MVIX" => self.compile_single_byte_arg(&mut result),
-            "MVIY" => self.compile_single_byte_arg(&mut result),
-            "MVIT" => self.compile_single_byte_arg(&mut result),
-            "MVAY" => self.compile_n_addr(1, &mut result),
-            "MVXA" => self.compile_two_bytes_arg(&mut result),
-            "MVYA" => self.compile_two_bytes_arg(&mut result),
-            "COPY" => result.extend(self.compile_copy().unwrap()),
-            "CPID" => self.compile_n_addr(2, &mut result),
-            "CPIR" => self.compile_n_addr(2, &mut result),
-            "ADDI" => self.compile_n_addr(3, &mut result),
-            "ADDX" => self.compile_single_byte_arg(&mut result),
-            "ADDY" => self.compile_single_byte_arg(&mut result),
-            "NEGI" => self.compile_n_addr(2, &mut result),
-            "EQLS" => self.compile_n_addr(2, &mut result),
-            "JUMP" => self.compile_two_bytes_arg(&mut result),
-            "FJMP" => self.compile_two_bytes_arg(&mut result),
-            "IJMP" => self.compile_two_bytes_arg(&mut result),
-            "WAIT" => result.push(self.op.opcode),
-            "CLRS" => result.push(self.op.opcode),
-            _ => panic!("Unknown instruction: {}", name)
-        }     
-
-        Ok(result)
-    }
-
-    pub fn compile_copy(&mut self) -> Result<Vec<u8>, String> {
+    pub fn compile_u8_u16(&mut self) -> Result<Vec<u8>, String> {
         let mut result = vec!();
         result.push(self.op.opcode);
 
@@ -77,6 +46,69 @@ impl Line {
         let addr = u16::from_str_radix(&self.args[1], 16).unwrap().as_u8_vec();
         result.extend(addr);
         
+        Ok(result)
+    }
+
+    pub fn to_compiled(&mut self) -> Result<Vec<u8>, String> {
+        let mut result = vec!();
+        let name: &str = &self.op.name.to_owned();
+        
+        match name {
+            "NOOP" => result.push(self.op.opcode),
+
+            "MVIX" => self.compile_single_byte_arg(&mut result),
+            "MVIY" => self.compile_single_byte_arg(&mut result),
+            "MVIT" => self.compile_single_byte_arg(&mut result),
+
+            "MVAX" => self.compile_n_addr(1, &mut result),
+            "MVAY" => self.compile_n_addr(1, &mut result),
+            "MVAT" => self.compile_n_addr(1, &mut result),
+
+            "MVXA" => self.compile_two_bytes_arg(&mut result),
+            "MVYA" => self.compile_two_bytes_arg(&mut result),
+            "MVTA" => self.compile_two_bytes_arg(&mut result),
+            "MVPA" => self.compile_two_bytes_arg(&mut result),
+            
+            "ADDX" => self.compile_single_byte_arg(&mut result),
+            "ADDY" => self.compile_single_byte_arg(&mut result),
+            "ADDT" => self.compile_single_byte_arg(&mut result),
+            "SUBX" => self.compile_single_byte_arg(&mut result),
+            "SUBY" => self.compile_single_byte_arg(&mut result),
+            "SUBT" => self.compile_single_byte_arg(&mut result),
+            
+            "COPY" => result.extend(self.compile_copy().unwrap()),
+            "CPID" => self.compile_n_addr(2, &mut result),
+            "CPIR" => self.compile_n_addr(2, &mut result),
+            
+            "ADDI" => self.compile_n_addr(3, &mut result),
+            "SUBI" => self.compile_n_addr(3, &mut result),
+            "MULI" => self.compile_n_addr(3, &mut result),
+            "DIVI" => self.compile_n_addr(3, &mut result),
+            "MODI" => self.compile_n_addr(3, &mut result),
+            "SWIZ" => self.compile_n_addr(3, &mut result),
+            "ANDI" => self.compile_n_addr(3, &mut result),
+            "ORLI" => self.compile_n_addr(3, &mut result),
+            "XORI" => self.compile_n_addr(3, &mut result),
+            "NEGI" => self.compile_n_addr(2, &mut result),
+            "SHLI" => self.compile_n_addr(2, &mut result),
+            "SHRI" => self.compile_n_addr(2, &mut result),
+
+            "EQLS" => self.compile_n_addr(2, &mut result),
+            "GRTR" => self.compile_n_addr(2, &mut result),
+            "LESS" => self.compile_n_addr(2, &mut result),
+            "JUMP" => self.compile_two_bytes_arg(&mut result),
+            "TJMP" => self.compile_two_bytes_arg(&mut result),
+            "FJMP" => self.compile_two_bytes_arg(&mut result),
+            "RJMP" => result.extend(self.compile_u8_u16().unwrap()),
+            "IJMP" => self.compile_two_bytes_arg(&mut result),
+
+            "BANK" => self.compile_single_byte_arg(&mut result),
+            "RAND" => self.compile_n_addr(2, &mut result),
+            "WAIT" => result.push(self.op.opcode),
+            "CLRS" => result.push(self.op.opcode),
+            _ => panic!("Unknown instruction: {}", name)
+        }     
+
         Ok(result)
     }
 }
